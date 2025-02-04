3e15792c4016147a10509890bd32839e8255612b
Robin <Robin@ROBIN_COMPUTER>
2025-02-04 16:29:09 -0500

    Add project files.

---
+++ Mutation_Testing/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Mutation_Testing
+{
+//Robinjit kaur
+namespace Mutation_Testing
+    {
+        public class Triangle
+        {
+            public static string ValidTriangle(int a, int b, int c) =>
+                (a + b > c && a + c > b && b + c > a)
+                    ? (a == b || b == c || a == c ? "The triangle is valid and isosceles." : "The triangle is valid.")
+                    : "The triangle is NOT valid.";
+        }
+    }
+
+    namespace Mutation_Testing
+    {
+        [TestFixture]
+        public class TriangleTests
+        {
+            [TestCase(70, 70, 40, "The triangle is valid and isosceles.")]
+            [TestCase(10, 5, 3, "The triangle is NOT valid.")]
+            public void Test_TriangleValidity(int a, int b, int c, string expected)
+            {
+                Assert.That(Triangle.ValidTriangle(a, b, c), Is.EqualTo(expected));
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//Robinjit kaur
+namespace Isosceles_Triangle
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            if ((firstAngle + secondAngle + thirdAngle) != 180)
+            {
+                return "The triangle is NOT valid.";
+            }
+
+            if (firstAngle == secondAngle || firstAngle == thirdAngle || secondAngle == thirdAngle)
+            {
+                return "The triangle is valid and isosceles.";
+            }
+
+            return "The triangle is valid but not isosceles.";
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstangle = 60;
+            int secondangle = 60;
+            int thirdangle = 60;
+
+            string result = Triangle.ValidTriangle(firstangle, secondangle, thirdangle);
+            Console.WriteLine(result);
+        }
+    }
+}
